4f24d485aa8cc07f8b808534ef3c68059accec3c
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-04-21 15:40:32 +0200

    Create main.go

---
+++ JS vc GO/GO/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func loop_thru(array []string) {
+	for i := 0; i < len(array); i++ {
+		fmt.Println("[FOR LOOP] ", array[i])
+	}
+
+	for _, value := range array {
+		fmt.Println("[FOR EACH LOOP] ", value)
+	}
+
+	fmt.Println("\n")
+
+	m1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for i, j := 0, len(m1)-1; i < j; i, j = i+1, j-1 {
+		m1[i], m1[j] = m1[j], m1[i]
+	}
+
+	fmt.Println(m1)
+}
+
+func time_devider(timer_one int64, timer_two int64) int64 {
+	return timer_one / timer_two
+}
+
+func timester(timer time.Time) int64 {
+	timester := timer.UnixNano()
+	millis := timester / 1000000
+
+	return millis
+}
+
+func main() {
+	timer_one := time.Now()
+
+	loop_thru(
+		[]string{
+			"This",
+			"is",
+			"that",
+			"and",
+			"that",
+			"is",
+			"this",
+			"and",
+			"I",
+			"am",
+			"not",
+			"possible",
+		},
+	)
+
+	timer_two := time.Now()
+
+	fmt.Print("[TIMER DEVISION] ", time_devider(timester(timer_two), timester(timer_one)))
+	fmt.Print("ms")
+}
